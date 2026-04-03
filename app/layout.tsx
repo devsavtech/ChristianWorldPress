d35da28c15f58d82df824de87146ffffaeb282e5
TheSamsDev <razasamee35@gmail.com>
2026-04-03 18:38:03 +0500

    new deisgn with animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,13 +54,42 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${crimsonText.variable} ${lato.variable}`}>
       <head>
-      
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=GTM-N8GZW322'+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N8GZW322');`,
+          }}
+        />
+        <Script
+          id="gtm-script2"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vwsmwdrxtq");`,
+          }}
+        />
       </head>
       <body
         className="font-sans antialiased bg-background text-foreground"
         suppressHydrationWarning
       >
-       
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N8GZW322"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <LiveChat />
         {children}
         {/* <ChatButton /> */}
